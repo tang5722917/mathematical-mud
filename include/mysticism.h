9481a1af95e367db32b673f453d9f8bb3220a05e
Tangzp <tang5722917@163.com>
2023-02-16 01:54:51 +0800

    建立游戏界面

---
+++ include/mysticism.h
@@ -0,0 +1,16 @@
+#ifndef MYSTICISM_H
+#define MYSTICISM_H
+
+/* 游戏目录 */
+#define PATH_DIR /"
+
+
+
+
+// 游戏管理员账号
+#define WIZARD  "mysticism"
+
+// 游戏欢迎界面
+#define MOTD    PATH_DIR "World/front-page/MOTD1"
+
+#endif
