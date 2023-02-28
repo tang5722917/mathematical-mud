3c072bdbdde388683d2b8423dfa519f77a3c6d07
Tangzp <tang5722917@163.com>
2023-02-28 08:27:34 +0800

    A little modificaiton

--- cmds/player/know.c
+++ cmds/player/know.c
@@ -1,8 +1,8 @@
 /*
  * @Author: Donald duck tang5722917@163.com
  * @Date: 2023-02-27 18:17:18
- * @LastEditors: Donald duck tang5722917@163.com
- * @LastEditTime: 2023-02-27 19:09:48
+ * @LastEditors: Tangzp tang5722917@163.com
+ * @LastEditTime: 2023-02-28 02:48:44
  * @FilePath: \mysticism-mud\cmds\player\know.c
  * @Description:  游戏中的知识界面
  *                包括冒险帮助与冒险经历
@@ -17,10 +17,8 @@ int know_commands(string command, object me);
 int main(object me, string arg)
 {
     object file;
-    me->disable_living();
     write("请输入查看的内容：\n") ;
-    input_to("know_commands",me);
-    me->enable_living();
+    get_char("know_commands",me);
     return 0;
 }
 
@@ -28,10 +26,10 @@ int know_commands(string command, object me)
 {
     if(command =="q")
     {
-        write("结束对已了解神秘知识的查看\n") ;
+        write("结束对已了解神秘知识的查看\n");
         return 2;
     }
     write("请输入继续查看的内容：\n") ;
-    input_to("know_commands",me);
+    get_char("know_commands",me);
     return 0;
 }
