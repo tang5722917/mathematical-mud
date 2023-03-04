1b8a315de3cd0e23ccd2706931e81593f2f4dab8
Tangzp <tang5722917@163.com>
2023-03-04 18:35:44 +0800

    development 2 2023/3/4

---
+++ World/world_create.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: Tangzp tang5722917@163.com
+ * @Date: 2023-03-04 18:27:27
+ * @LastEditors: Tangzp tang5722917@163.com
+ * @LastEditTime: 2023-03-04 18:31:49
+ * @FilePath: \mysticism-mud\World\world_create.c
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by tang5722917@163.com, All Rights Reserved. 
+ */
+
+
+int create(int version)
+{
+    
+}
