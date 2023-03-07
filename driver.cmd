5b02b6175f74398ce87ac75a24dfcd7815b4b79b
Donald duck <tang5722917@163.com>
2023-03-07 19:56:43 +0800

    A little modification

---
+++ driver.cmd
@@ -0,0 +1,13 @@
+@echo off
+color 02
+title X@Mud.Ren
+::环境配置
+set MUDLIB="%cd%"
+set DRIVER="%cd%\bin\driver.exe"
+set CONFIG="%cd%\config.cfg"
+::运行游戏
+:START
+cd %MUDLIB%
+%DRIVER% %CONFIG% %*
+::goto START
+pause
--- system/kernel/master.c
+++ system/kernel/master.c
@@ -1,11 +1,11 @@
 inherit CORE_MASTER_OB;
-
 // 玩家连线入口方法，返回连线对象
 private object connect(int port)
 {
     object login_ob;
     mixed err;
 
+    
     err = catch (login_ob = new(MYSTICISM_LOGIN_OB));
     if (err)
     {
@@ -19,4 +19,22 @@ private void create()
 {
     debug_message("[" + ctime() + "]CORE_MASTER_OB->create()!");
     preload(Game_World);
+}
+
+// driver 启动测试
+void flag(string str)
+{
+    debug_message("[" + ctime() + "]"+str+"\n");
+    switch (str)
+    {
+    case "debug":
+        store_variable("DEBUG", 1);
+        break;
+    case "auto-test":
+        
+        break;
+    default:
+        write("[CORE_MASTER_OB]->flag():The only supproted flag is 'debug', got '" + str + "'.\n");
+    }
+    // otherwise wait for auto shutdown
 }
