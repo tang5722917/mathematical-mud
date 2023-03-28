e71a5ec11f5089f46aed6e0850731363307dc9b9
Tangzp <tang5722917@163.com>
2023-03-28 20:30:01 +0800

    A little modification

---
+++ World/00/start_mxp.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: Tangzp tang5722917@163.com
+ * @Date: 2023-03-28 00:12:46
+ * @LastEditors: Tangzp tang5722917@163.com
+ * @LastEditTime: 2023-03-28 05:03:40
+ * @FilePath: \mysticism-mud\World\00\start_mxp.c
+ * @Description: 玩家登录MXP检查
+ * Copyright (c) 2023 by Tangzp email: tang5722917@163.com, All Rights Reserved.
+ */
+
+inherit CORE_CONDITION_MOD;
+inherit _MXP;
+
+void heart_beat_effect(object ob)
+{
+    write("Hello World\n");
+}
--- system/daemons/login_d.c
+++ system/daemons/login_d.c
@@ -2,7 +2,7 @@
  * @Author: Donald duck tang5722917@163.com
  * @Date: 2023-02-17 10:19:27
  * @LastEditors: Tangzp tang5722917@163.com
- * @LastEditTime: 2023-02-25 06:09:14
+ * @LastEditTime: 2023-03-28 08:16:50
  * @FilePath: \mysticism-mud\system\daemons\login_d.c
  * @Description: 
  * 
@@ -17,4 +17,31 @@ protected void welcome(object ob)
     write("\n                               版本"VERSION);
     write("\n^_^!请输入你的登录ID:");
     input_to("get_id", ob);
+}
+
+// 进入游戏
+void enter_world(object ob, object user)
+{
+    object MXP_Init;
+#ifdef START_ROOM
+    string start_room = START_ROOM;
+#else
+    string start_room = VOID_OB;
+#endif
+    user->set_temp("login_ob", ob);
+    ob->set_temp("user_ob", user);
+    if (interactive(ob))
+        exec(user, ob);
+
+    user->setup(); // 激活玩家角色
+    user->set("last_login_ip", ob->query_temp("ip_number"));
+    user->set("last_login_at", time());
+    user->set("last_saved_at", time());
+    user->add("login_times", 1);
+    MXP_Init = new("/World/00/start_mxp");
+    //user->start_condition("MXP_Init",5);
+    user->save(); // 保存玩家数据
+
+    user->move(start_room);
+    tell_room(start_room, user->short() + "连线进入这个世界。\n", ({user}));
 }
