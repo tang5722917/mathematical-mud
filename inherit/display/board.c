b92a2d5275e951e0ac75af62a9e3e4a6808dbd10
Donald duck <tang5722917@163.com>
2023-04-07 18:15:11 +0800

    重新整理inherit文件分类

---
+++ inherit/display/board.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: Donald duck tang5722917@163.com
+ * @Date: 2023-04-07 17:33:46
+ * @LastEditors: Donald duck tang5722917@163.com
+ * @LastEditTime: 2023-04-07 17:34:00
+ * @FilePath: \mysticism-mud\inherit\board.c
+ * @Description: 各种方形显示模板
+ * Copyright (c) 2023 by Donald duck email: tang5722917@163.com, All Rights Reserved.
+ */
+
+
+l
