cab549e0d900aa9cd62306146f0b1fb576f9d375
Donald duck <tang5722917@163.com>
2023-04-13 14:05:40 +0800

    完成MUD启动SQLite数据库初始化开发 建立初始SQLite模板

--- system/daemons/db_d.c
+++ system/daemons/db_d.c
@@ -2,9 +2,12 @@
  * @Author: Donald duck tang5722917@163.com
  * @Date: 2023-04-12 18:21:31
  * @LastEditors: Donald duck tang5722917@163.com
- * @LastEditTime: 2023-04-12 18:23:19
+ * @LastEditTime: 2023-04-13 11:40:45
  * @FilePath: \mysticism-mud\system\daemons\db_d.c
  * @Description: 数据库守护进程
  * Copyright (c) 2023 by Donald duck email: tang5722917@163.com, All Rights Reserved.
  */
  inherit CORE_DB;
+
+mixed get_db_handle(){return db_handle;}
+string * get_db_table_column(){return db_table_column;}
--- system/kernel/master/db.c
+++ system/kernel/master/db.c
@@ -2,7 +2,7 @@
  * @Author: Donald duck tang5722917@163.com
  * @Date: 2023-04-12 18:10:48
  * @LastEditors: Donald duck tang5722917@163.com
- * @LastEditTime: 2023-04-12 19:34:43
+ * @LastEditTime: 2023-04-13 14:02:39
  * @FilePath: \mysticism-mud\system\kernel\master\db.c
  * @Description: 数据库初始化函数
  * Copyright (c) 2023 by Donald duck email: tang5722917@163.com, All Rights Reserved.
@@ -11,8 +11,9 @@
  int init_sqlite3()
  {
     object ob,db;
-    string db_host;
-    mixed db_hander;
+    string db_host,str, *s;
+    mixed db_db,rows,*f,db_handle;
+    mapping db_config=([]);
     ob = find_object(CORE_ENV_D);
     db = load_object(DB_D);
     if(db == 0)
@@ -29,12 +30,23 @@
     debug_message("[" + ctime() + "]当前游戏数据库名："+ob->query("DB_DATABASE"));
     if(ob->query("DB_TYPE") == "SQLite")
     {
-        debug_message("[" + ctime() + "]当前游戏数据名地址："+ob->query("DB_PATH"));
-        db_host = ob->query("DB_PATH")+ob->query("DB_DATABASE")+".db";
-        db_hander = db_connect("", db_host, "", __USE_SQLITE3__);
+        debug_message("[" + ctime() + "]当前游戏数据库路径："+ob->query("DB_PATH"));
+        db_db = ob->query("DB_PATH")+ob->query("DB_DATABASE")+".db";
+        db_config["host"]="";
+        db_config["database"]=db_db;
+        db_config["user"]="";
+        db_config["type"]=__USE_SQLITE3__;
+        db->setConnection(db_config);
+        db->exec();
         if (stringp(db)) /* error */
             debug_message("[" + ctime() + "]数据库连接失败：" + db);
         else
             debug_message("[" + ctime() + "]数据库连接成功：" + db);
     }
+    debug_message("[" + ctime() + "]数据库状态：" + db_status());
+    db_handle=db->get_db_handle();
+    rows = db_exec(db_handle,"select * from DB_Info;");
+    f = db_fetch(db_handle,1);
+    debug_message("[" + ctime() + "]Mysticism-MUD数据库版本：" + f[0]);
+    debug_message("[" + ctime() + "]Mysticism-MUD数据库版本说明:" + f[1]);
 }
