38c9eb8aab1bcda5f4cc8efc6cd208146c5260e9
Donald duck <tang5722917@163.com>
2023-04-20 20:31:26 +0800

    修正2处错误

--- inherit/system/error.c
+++ inherit/system/error.c
@@ -2,7 +2,7 @@
  * @Author: Donald duck tang5722917@163.com
  * @Date: 2023-04-10 19:53:19
  * @LastEditors: Donald duck tang5722917@163.com
- * @LastEditTime: 2023-04-20 19:49:01
+ * @LastEditTime: 2023-04-20 20:31:21
  * @FilePath: \mysticism-mud\inherit\system\error.c
  * @Description: 错误处理基类,整合常见错误类型
  * Copyright (c) 2023 by Donald duck email: tang5722917@163.com, All Rights Reserved.
@@ -50,7 +50,7 @@ int is_living(object ob)
 
 int is_entity(object ob)
 {
-    if(!objectp(ob))
+    if(objectp(ob))
         if(inherits(INHERIT_PATH "entity/entity",ob))
             return 0;
     err = catch{
