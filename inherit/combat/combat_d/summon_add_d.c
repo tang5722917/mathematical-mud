0ca6061220cfeb12af38558243c2d895db7a7cec
Donald Duck <tang5722917@163.com>
2023-05-13 19:25:22 +0800

    召唤物开发

--- inherit/combat/combat_d/summon_add_d.c
+++ inherit/combat/combat_d/summon_add_d.c
@@ -2,13 +2,29 @@
  * @Author: Donald duck tang5722917@163.com
  * @Date: 2023-05-04 17:43:41
  * @LastEditors: Donald Duck tang5722917@163.com
- * @LastEditTime: 2023-05-09 08:23:47
+ * @LastEditTime: 2023-05-13 19:12:24
  * @FilePath: /mysticism-mud/inherit/combat/combat_d/summon_add_d.c
  * @Description: 新增/初始化召唤物
  * Copyright (c) 2023 by Donald duck email: tang5722917@163.com, All Rights Reserved.
  */
+#include <summon.h>
+
+string summon_place(int n)
+{
+    if( n < 10 )
+        return "我方召唤区"
+}
 
 string add_summon(object *user,object card_ob,object combat)
 {
-    return card_ob->card_effect_descript();
+    string str="";
+    object *su,su_data;
+    su = card_ob->perform_summon();
+    s += WHT+"使用"+card_ob->card_name()+"牌召唤出"+sizeof(su)+"个召唤物"+ NOR "\n";
+    foreach(object o in su){
+        s += WHT +"召唤出"+o->summon_name()"置于"+summon_place()+NOR "\n";
+        su_data = o->init_summon_data();
+        
+    }
+    return str;
 }
--- inherit/entity/summon_data.c
+++ inherit/entity/summon_data.c
@@ -1,9 +1,9 @@
 /*
  * @Author: Donald duck tang5722917@163.com
  * @Date: 2023-04-28 16:49:25
- * @LastEditors: Donald duck tang5722917@163.com
- * @LastEditTime: 2023-04-28 17:15:37
- * @FilePath: \mysticism-mud\inherit\summon\summon_data.c
+ * @LastEditors: Donald Duck tang5722917@163.com
+ * @LastEditTime: 2023-05-13 19:09:59
+ * @FilePath: /mysticism-mud/inherit/entity/summon_data.c
  * @Description: 召唤物属性类，处理战斗过程中召唤物的数值变化
  * Copyright (c) 2023 by Donald duck email: tang5722917@163.com, All Rights Reserved.
  */
@@ -12,19 +12,27 @@
 */
 inherit _CLEAN_UP;
 
-nosave protected int hp,type;
+nosave protected int type;
 nosave protected object dbase;
+nosave protected object summon;
 
-varargs void init(int hp_in,int type_in,mapping status)
+object get_summon_card(){return card;}
+object get_summon_dbase(){return dbase;}
+
+varargs void init(int type_in,mapping status,object su)
 {
-    hp = hp_in;
     type = type_in;
-    if(status !=0 )
+    summon = su;
+    if( (mapp(status)) && (sizeof(status)))
     {
         dbase = new(_DBASE);
+        foreach(mixed k,mixed v in status)
+            dbase->set(k,v);
     }
 }
 
+mixed query(string key){return dbase->query(key);}
+void set(string key,mixed value){dbase->set(key,value);}
 int remove()
 {
     if(dbase)
