de6128cb6a82f3c057c385cfe1bc31ca122825ad
Donald Duck <tang5722917@163.com>
2023-05-21 02:28:41 +0800

    开始进行地图开发

--- World/00/map/1/area_1.c
+++ World/00/map/1/area_1.c
@@ -2,7 +2,7 @@
  * @Author: Donald Duck tang5722917@163.com
  * @Date: 2023-05-20 05:55:52
  * @LastEditors: Donald Duck tang5722917@163.com
- * @LastEditTime: 2023-05-20 06:31:41
+ * @LastEditTime: 2023-05-20 10:42:52
  * @FilePath: /mysticism-mud/World/00/map/1/area_1.c
  * @Description: 区域1，鲁恩/廷根/ 黑荆棘安保公司-圣塞琳娜教堂
  * Copyright (c) 2023 by Donald Duck email: tang5722917@163.com, All Rights Reserved.
@@ -15,13 +15,17 @@
                        |
 (通往教堂地下)告解室 —— 教堂大厅
 
-                    队长办公室
+                 队长办公室  休息室
+                       |   /
+（通往地下一层）办公室 —— 走廊 —— 会计室
                        |
-        （通往地下一层）办公室 —— 走廊 —— 会计室
+                      前台（黑荆棘安保公司）
                        |
-                      前台
+                    佐特兰街
                        |
-                     佐特兰街
+                    贝西克街
+                       |
+                    猎犬酒吧
 地下一层
                   武器室  停尸房
                      |  /
---
+++ client/readme.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: Donald Duck tang5722917@163.com
+ * @Date: 2023-05-21 02:26:44
+ * @LastEditors: Donald Duck tang5722917@163.com
+ * @LastEditTime: 2023-05-21 02:27:23
+ * @FilePath: /mysticism-mud/client/readme.md
+ * @Description: 和游戏client相关的说明
+ * Copyright (c) 2023 by Donald Duck email: tang5722917@163.com, All Rights Reserved.
+-->
